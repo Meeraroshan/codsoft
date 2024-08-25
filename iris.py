0bd712b16a709bcebb6d8bce1f655db75c929be5
Meeraroshan <153701384+Meeraroshan@users.noreply.github.com>
2024-08-25 12:27:25 +0530

    Add files via upload

--- iris.py
+++ iris.py
@@ -1,37 +1,48 @@
+
+#IRIS TASK
+import numpy as np
 import pandas as pd
+from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-
-titanic_data = pd.read_csv('Titanic-Dataset.csv')
-
-print(titanic_data.head())
-print('\n')
-print(titanic_data.info())
-print('\n')
-
-titanic_data.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1, inplace=True)
-
-titanic_data['Age'] = titanic_data['Age'].fillna(titanic_data['Age'].median())
-titanic_data['Embarked'] = titanic_data['Embarked'].fillna(titanic_data['Embarked'].mode()[0])
-
-
-titanic_data = pd.get_dummies(titanic_data, columns=['Sex', 'Embarked'], drop_first=True)
-
-X = titanic_data.drop('Survived', axis=1)
-y = titanic_data['Survived']
-
+from sklearn.metrics import accuracy_score, classification_report
+import matplotlib.pyplot as plt
+import seaborn as sns
+iris = load_iris()
+X = iris.data  
+y = iris.target 
+data = pd.read_csv('IRIS.csv')
+print(data.sample(2))
+print(data.columns)
+print("Shape:", data.shape)
+numeric_data = data.select_dtypes(include=np.number)
+print("Mean:")
+print(numeric_data.mean())
+numeric_data = data.select_dtypes(include=np.number)
+print("Median:")
+print(numeric_data.median())
+print("\nSum:")
+print(data.sum())
+print("\nMinimum:")
+print(data.min())
+print("\nMaximum:")
+print(data.max())
+print(data.iloc[5])
+filtered_data = data.loc[data["species"] == "Iris-setosa"]
+print(filtered_data)
+plt.figure(figsize=(10, 6))
+data['species'].value_counts().plot(kind='bar', color='green')
+plt.title('Distribution of Iris Species')
+plt.xlabel('Species')
+plt.ylabel('Number of Instances')
+plt.xticks(rotation=45)
+plt.grid(axis='y')
+plt.show()
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-model = RandomForestClassifier(n_estimators=100, random_state=42)
+model = RandomForestClassifier()
 model.fit(X_train, y_train)
-
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
-print('Accuracy:', accuracy)
-print()
-print('Classification Report:')
+print("Accuracy:", accuracy)
+print("Classification Report:")
 print(classification_report(y_test, y_pred))
-print()
-print('Confusion Matrix:')
-print(confusion_matrix(y_test, y_pred))
